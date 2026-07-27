d8d78f31171250bb28f868afc62400b5d63b13ae
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2026-07-27 19:03:20 +0200

    Completely remove aurorae themes

--- install.sh
+++ install.sh
@@ -5,13 +5,11 @@ ROOT_UID=0
 
 # Destination directory
 if [ "$UID" -eq "$ROOT_UID" ]; then
-    AURORAE_DIR="/usr/share/aurorae/themes"
     SCHEMES_DIR="/usr/share/color-schemes"
     PLASMA_DIR="/usr/share/plasma/desktoptheme"
     LOOKFEEL_DIR="/usr/share/plasma/look-and-feel"
     KVANTUM_DIR="/usr/share/Kvantum"
 else
-    AURORAE_DIR="$HOME/.local/share/aurorae/themes"
     SCHEMES_DIR="$HOME/.local/share/color-schemes"
     PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
     LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
@@ -20,7 +18,6 @@ fi
 
 THEME_NAME=Onyx
 
-[[ ! -d ${AURORAE_DIR} ]] && mkdir -p ${AURORAE_DIR}
 [[ ! -d ${SCHEMES_DIR} ]] && mkdir -p ${SCHEMES_DIR}
 [[ ! -d ${PLASMA_DIR} ]] && mkdir -p ${PLASMA_DIR}
 [[ ! -d ${LOOKFEEL_DIR} ]] && mkdir -p ${LOOKFEEL_DIR}
@@ -29,13 +26,11 @@ THEME_NAME=Onyx
 install() {
     local name=${1}
 
-    [[ -d ${AURORAE_DIR}/${name} ]] && rm -rf ${AURORAE_DIR}/${name}*
     [[ -d ${PLASMA_DIR}/${name} ]] && rm -rf ${PLASMA_DIR}/${name}*
     [[ -f ${SCHEMES_DIR}/${name}.colors ]] && rm -rf ${SCHEMES_DIR}/${name}*.colors
     [[ -d ${LOOKFEEL_DIR}/com.github.vinceliuice.${name} ]] && rm -rf ${LOOKFEEL_DIR}/com.github.vinceliuice.${name}*
     [[ -d ${KVANTUM_DIR}/${name} ]] && rm -rf ${KVANTUM_DIR}/${name}*
 
-    cp -r ${SRC_DIR}/aurorae/*                                                         ${AURORAE_DIR}
     cp -r ${SRC_DIR}/color-schemes/*.colors                                            ${SCHEMES_DIR}
     cp -r ${SRC_DIR}/kvantum/*                                                         ${KVANTUM_DIR}
     cp -r ${SRC_DIR}/plasma/desktoptheme/${name}                                       ${PLASMA_DIR}
